e387c623218183b3257df5432595043efc637a5d
glowkeeper <steve.huckle@gmail.com>
2023-03-06 14:55:28 +0000

    register

--- CommentStore/settings.py
+++ CommentStore/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
+    'crispy_bootstrap5',
     'CommentStore'
 ]
 
@@ -122,3 +124,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_TEMPLATE_PACK = "bootstrap5" 
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5" 
--- CommentStore/urls.py
+++ CommentStore/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+from register import views as register_views 
+
 urlpatterns = [
     path('commentstore/', include('commentstoreapp.urls')),
     path('admin/', admin.site.urls),
+    path("register/", register_views.register_user, name="register")
 ]
