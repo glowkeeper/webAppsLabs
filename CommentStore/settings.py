93ad6850268f272e5394d139483024c6249c0567
steve <steve.huckle@gmail.com>
2023-03-08 13:57:13 +0000

    add sqllite database

--- CommentStore/settings.py
+++ CommentStore/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'crispy_forms',
     'crispy_bootstrap5',
-    'CommentStore'
+    'CommentStore',
+    'commentstoreapp.apps.CommentstoreappConfig'
 ]
 
 MIDDLEWARE = [
@@ -106,11 +107,12 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'en-gb'
 
 TIME_ZONE = 'UTC'
 
 USE_I18N = True
+USE_L10N = True
 
 USE_TZ = True
 
--- commentstoreapp/views.py
+++ commentstoreapp/views.py
@@ -1,18 +1,14 @@
-from django.http import HttpResponse
 from django.shortcuts import render, redirect
-from commentstoreapp.commentstore import CommentStore
-from commentstoreapp.forms import InsertNewComment
-from datetime import datetime
 from django.views.decorators.csrf import csrf_exempt
-import logging
 
-logging.basicConfig(level=logging.INFO)
+from commentstoreapp.forms import InsertNewComment
+from commentstoreapp.models import Comment
+
+# import logging
+# logging.basicConfig(level=logging.INFO)
 
 @csrf_exempt
 def commentstore(request):
-
-    store = CommentStore()
-    cmnt_list = []
 
     if request.method == 'POST':
         form = InsertNewComment(request.POST)
@@ -21,10 +17,13 @@ def commentstore(request):
             n = form.cleaned_data["name"]
             c = form.cleaned_data["comment_str"]
             d = form.cleaned_data["visit_date"]
-            store.insertcomment(n, d, c)
-            cmnt_list = list(store.commentlist.queue)
+            t = Comment(name=n, visit_date=d, comment_str=c)
+            t.save()
+            
+            # for cmnt in cmnt_list:
+            #    print(cmnt.name + " : " + cmnt.visit_date.__str__() + " : " + cmnt.comment_str)
 
-        return redirect("home")
+            return redirect("home")
     else:
         form = InsertNewComment()
 
@@ -32,7 +31,5 @@ def commentstore(request):
 
 
 def home(request):
-    # logging.info("home:")
-    store = CommentStore()
-    cmnt_list = list(store.commentlist.queue)
-    return render(request, "commentstore/home.html", {'cmnt_list': cmnt_list})
+    cmnt_list = list(Comment.objects.all())
+    return render(request, "commentstore/home.html", {"cmnt_list": cmnt_list})
